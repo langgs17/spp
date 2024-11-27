7933315a0a5b18136c47a352a249a655746ac99d
langgs17 <anggita030321@gmail.com>
2024-11-27 21:11:16 +0700

    db

--- webspp.sql
+++ webspp.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 01 Agu 2022 pada 11.58
--- Versi server: 10.4.20-MariaDB
--- Versi PHP: 7.3.29
+-- Waktu pembuatan: 27 Nov 2024 pada 15.09
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,15 +31,16 @@ CREATE TABLE `admin` (
   `id_admin` int(11) NOT NULL,
   `nama_admin` varchar(50) NOT NULL,
   `user_admin` varchar(50) NOT NULL,
-  `pass_admin` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `pass_admin` varchar(50) NOT NULL,
+  `level` enum('admin','user') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `admin`
 --
 
-INSERT INTO `admin` (`id_admin`, `nama_admin`, `user_admin`, `pass_admin`) VALUES
-(5, 'Siti Fatimah', 'siti', 'admin');
+INSERT INTO `admin` (`id_admin`, `nama_admin`, `user_admin`, `pass_admin`, `level`) VALUES
+(5, 'Siti Fatimah', 'siti', 'admin', 'admin');
 
 -- --------------------------------------------------------
 
@@ -51,7 +52,7 @@ CREATE TABLE `angkatan` (
   `id_angkatan` int(11) NOT NULL,
   `nama_angkatan` varchar(50) NOT NULL,
   `biaya` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `angkatan`
@@ -71,7 +72,7 @@ INSERT INTO `angkatan` (`id_angkatan`, `nama_angkatan`, `biaya`) VALUES
 CREATE TABLE `jurusan` (
   `id_jurusan` int(11) NOT NULL,
   `nama_jurusan` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `jurusan`
@@ -79,7 +80,8 @@ CREATE TABLE `jurusan` (
 
 INSERT INTO `jurusan` (`id_jurusan`, `nama_jurusan`) VALUES
 (1, 'Multimedia'),
-(2, 'Akuntansi Keuangan Dan Lembaga');
+(2, 'Akuntansi Keuangan Dan Lembaga'),
+(4, 'Rekayasa Perangkat Lunak');
 
 -- --------------------------------------------------------
 
@@ -91,7 +93,7 @@ CREATE TABLE `kelas` (
   `id_kelas` int(11) NOT NULL,
   `nama_kelas` varchar(50) NOT NULL,
   `kelas` int(2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `kelas`
@@ -109,7 +111,22 @@ INSERT INTO `kelas` (`id_kelas`, `nama_kelas`, `kelas`) VALUES
 (12, 'XII AKL 1', 12),
 (13, 'XII AKL 2', 12),
 (14, 'XII MM 1', 12),
-(15, 'XII MM 2', 12);
+(15, 'XII MM 2', 12),
+(16, 'X PPLG', 10),
+(17, 'XI PPLG', 11),
+(18, 'XII RPL', 12);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kepala_sekolah`
+--
+
+CREATE TABLE `kepala_sekolah` (
+  `id_kepsek` int(11) NOT NULL,
+  `nama_kepsek` varchar(50) NOT NULL,
+  `periode` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -129,61 +146,51 @@ CREATE TABLE `pembayaran` (
   `id_admin` int(11) NOT NULL,
   `tahun` int(4) NOT NULL,
   `kelas` int(2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `pembayaran`
 --
 
 INSERT INTO `pembayaran` (`idspp`, `id_siswa`, `jatuhtempo`, `bulan`, `nobayar`, `tglbayar`, `jumlah`, `ket`, `id_admin`, `tahun`, `kelas`) VALUES
-(1, 1, '2020', 'Juli 2020', '010820221136443644', '2022-08-01', '250000', 'LUNAS', 0, 2020, 10),
-(2, 1, '2020', 'Agustus 2020', '010820221143474347', '2022-08-01', '250000', 'LUNAS', 0, 2020, 10),
-(3, 1, '2020', 'September 2020', '010820221148564856', '2022-08-01', '250000', 'LUNAS', 0, 2020, 10),
-(4, 1, '2020', 'Oktober 2020', '010820221148584858', '2022-08-01', '250000', 'LUNAS', 0, 2020, 10),
-(5, 1, '2020', 'November 2020', '010820221157335733', '2022-08-01', '250000', 'LUNAS', 5, 2020, 10),
-(6, 1, '2020', 'Desember 2020', '', '', '250000', 'BELUM DIBAYAR', 0, 2020, 10),
-(7, 1, '2021', 'Januari 2021', '010820221158075807', '2022-08-01', '250000', 'LUNAS', 5, 2020, 10),
-(8, 1, '2021', 'Februari 2021', '', '', '250000', 'BELUM DIBAYAR', 0, 2020, 10),
-(9, 1, '2021', 'Maret 2021', '', '', '250000', 'BELUM DIBAYAR', 0, 2020, 10),
-(10, 1, '2021', 'April 2021', '010820221156145614', '2022-08-01', '250000', 'LUNAS', 5, 2020, 10),
-(11, 1, '2021', 'Mei 2021', '', '', '250000', 'BELUM DIBAYAR', 0, 2020, 10),
-(12, 1, '2021', 'Juni 2021', '', '', '250000', 'BELUM DIBAYAR', 0, 2020, 10),
-(13, 2, '2020', 'Juli 2020', '010820221137033703', '2022-08-01', '250000', 'LUNAS', 0, 2020, 11),
-(14, 2, '2020', 'Agustus 2020', '010820221137053705', '2022-08-01', '250000', 'LUNAS', 0, 2020, 11),
-(15, 2, '2020', 'September 2020', '010820221137073707', '2022-08-01', '250000', 'LUNAS', 0, 2020, 11),
-(16, 2, '2020', 'Oktober 2020', '010820221138233823', '2022-08-01', '250000', 'LUNAS', 0, 2020, 11),
-(17, 2, '2020', 'November 2020', '010820221138263826', '2022-08-01', '250000', 'LUNAS', 0, 2020, 11),
-(18, 2, '2020', 'Desember 2020', '010820221138323832', '2022-08-01', '250000', 'LUNAS', 0, 2020, 11),
-(19, 2, '2021', 'Januari 2021', '010820221138303830', '2022-08-01', '250000', 'LUNAS', 0, 2020, 11),
-(20, 2, '2021', 'Februari 2021', '010820221138283828', '2022-08-01', '250000', 'LUNAS', 0, 2020, 11),
-(21, 2, '2021', 'Maret 2021', '010820221138343834', '2022-08-01', '250000', 'LUNAS', 0, 2020, 11),
-(22, 2, '2021', 'April 2021', '010820221138363836', '2022-08-01', '250000', 'LUNAS', 0, 2020, 11),
-(23, 2, '2021', 'Mei 2021', '010820221138383838', '2022-08-01', '250000', 'LUNAS', 0, 2020, 11),
-(24, 2, '2021', 'Juni 2021', '010820221138403840', '2022-08-01', '250000', 'LUNAS', 0, 2020, 11),
-(25, 2, '2021', 'Juli 2021', '010820221139473947', '2022-08-01', '275000', 'LUNAS', 0, 2021, 12),
-(26, 2, '2021', 'Agustus 2021', '010820221148404840', '2022-08-01', '275000', 'LUNAS', 0, 2021, 12),
-(27, 2, '2021', 'September 2021', '010820221143314331', '2022-08-01', '275000', 'LUNAS', 0, 2021, 12),
-(28, 2, '2021', 'Oktober 2021', '', '', '275000', 'BELUM DIBAYAR', 0, 2021, 12),
-(29, 2, '2021', 'November 2021', '', '', '275000', 'BELUM DIBAYAR', 0, 2021, 12),
-(30, 2, '2021', 'Desember 2021', '', '', '275000', 'BELUM DIBAYAR', 0, 2021, 12),
-(31, 2, '2022', 'Januari 2022', '', '', '275000', 'BELUM DIBAYAR', 0, 2021, 12),
-(32, 2, '2022', 'Februari 2022', '', '', '275000', 'BELUM DIBAYAR', 0, 2021, 12),
-(33, 2, '2022', 'Maret 2022', '', '', '275000', 'BELUM DIBAYAR', 0, 2021, 12),
-(34, 2, '2022', 'April 2022', '', '', '275000', 'BELUM DIBAYAR', 0, 2021, 12),
-(35, 2, '2022', 'Mei 2022', '', '', '275000', 'BELUM DIBAYAR', 0, 2021, 12),
-(36, 2, '2022', 'Juni 2022', '', '', '275000', 'BELUM DIBAYAR', 0, 2021, 12),
-(37, 3, '2021', 'Juli 2021', '010820221156525652', '2022-08-01', '275000', 'LUNAS', 5, 2021, 12),
-(38, 3, '2021', 'Agustus 2021', '010820221156545654', '2022-08-01', '275000', 'LUNAS', 5, 2021, 12),
-(39, 3, '2021', 'September 2021', '', '', '275000', 'BELUM DIBAYAR', 0, 2021, 12),
-(40, 3, '2021', 'Oktober 2021', '', '', '275000', 'BELUM DIBAYAR', 0, 2021, 12),
-(41, 3, '2021', 'November 2021', '', '', '275000', 'BELUM DIBAYAR', 0, 2021, 12),
-(42, 3, '2021', 'Desember 2021', '', '', '275000', 'BELUM DIBAYAR', 0, 2021, 12),
-(43, 3, '2022', 'Januari 2022', '', '', '275000', 'BELUM DIBAYAR', 0, 2021, 12),
-(44, 3, '2022', 'Februari 2022', '', '', '275000', 'BELUM DIBAYAR', 0, 2021, 12),
-(45, 3, '2022', 'Maret 2022', '', '', '275000', 'BELUM DIBAYAR', 0, 2021, 12),
-(46, 3, '2022', 'April 2022', '', '', '275000', 'BELUM DIBAYAR', 0, 2021, 12),
-(47, 3, '2022', 'Mei 2022', '', '', '275000', 'BELUM DIBAYAR', 0, 2021, 12),
-(48, 3, '2022', 'Juni 2022', '', '', '275000', 'BELUM DIBAYAR', 0, 2021, 12);
+(133, 11, '2021', 'Juli 2021', '', '', '275000', 'BELUM DIBAYAR', 0, 2021, 11),
+(134, 11, '2021', 'Agustus 2021', '', '', '275000', 'BELUM DIBAYAR', 0, 2021, 11),
+(135, 11, '2021', 'September 2021', '', '', '275000', 'BELUM DIBAYAR', 0, 2021, 11),
+(136, 11, '2021', 'Oktober 2021', '', '', '275000', 'BELUM DIBAYAR', 0, 2021, 11),
+(137, 11, '2021', 'November 2021', '', '', '275000', 'BELUM DIBAYAR', 0, 2021, 11),
+(138, 11, '2021', 'Desember 2021', '', '', '275000', 'BELUM DIBAYAR', 0, 2021, 11),
+(139, 11, '2022', 'Januari 2022', '', '', '275000', 'BELUM DIBAYAR', 0, 2021, 11),
+(140, 11, '2022', 'Februari 2022', '', '', '275000', 'BELUM DIBAYAR', 0, 2021, 11),
+(141, 11, '2022', 'Maret 2022', '', '', '275000', 'BELUM DIBAYAR', 0, 2021, 11),
+(142, 11, '2022', 'April 2022', '', '', '275000', 'BELUM DIBAYAR', 0, 2021, 11),
+(143, 11, '2022', 'Mei 2022', '', '', '275000', 'BELUM DIBAYAR', 0, 2021, 11),
+(144, 11, '2022', 'Juni 2022', '', '', '275000', 'BELUM DIBAYAR', 0, 2021, 11),
+(169, 14, '2020', 'Juli 2020', '', '', '250000', 'BELUM DIBAYAR', 0, 2020, 12),
+(170, 14, '2020', 'Agustus 2020', '', '', '250000', 'BELUM DIBAYAR', 0, 2020, 12),
+(171, 14, '2020', 'September 2020', '', '', '250000', 'BELUM DIBAYAR', 0, 2020, 12),
+(172, 14, '2020', 'Oktober 2020', '', '', '250000', 'BELUM DIBAYAR', 0, 2020, 12),
+(173, 14, '2020', 'November 2020', '', '', '250000', 'BELUM DIBAYAR', 0, 2020, 12),
+(174, 14, '2020', 'Desember 2020', '', '', '250000', 'BELUM DIBAYAR', 0, 2020, 12),
+(175, 14, '2021', 'Januari 2021', '', '', '250000', 'BELUM DIBAYAR', 0, 2020, 12),
+(176, 14, '2021', 'Februari 2021', '', '', '250000', 'BELUM DIBAYAR', 0, 2020, 12),
+(177, 14, '2021', 'Maret 2021', '', '', '250000', 'BELUM DIBAYAR', 0, 2020, 12),
+(178, 14, '2021', 'April 2021', '', '', '250000', 'BELUM DIBAYAR', 0, 2020, 12),
+(179, 14, '2021', 'Mei 2021', '', '', '250000', 'BELUM DIBAYAR', 0, 2020, 12),
+(180, 14, '2021', 'Juni 2021', '', '', '250000', 'BELUM DIBAYAR', 0, 2020, 12);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pengeluaran`
+--
+
+CREATE TABLE `pengeluaran` (
+  `id_pengeluaran` int(11) NOT NULL,
+  `id_admin` int(11) NOT NULL,
+  `keterangan` varchar(50) NOT NULL,
+  `pembayaran` enum('lunas','belum bayar') NOT NULL,
+  `total` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -198,17 +205,17 @@ CREATE TABLE `siswa` (
   `id_angkatan` varchar(50) NOT NULL,
   `id_jurusan` int(11) NOT NULL,
   `id_kelas` int(11) NOT NULL,
-  `alamat` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `alamat` text NOT NULL,
+  `jenis_kelamin` enum('laki-laki','perempuan') NOT NULL,
+  `ttl` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `siswa`
 --
 
-INSERT INTO `siswa` (`id_siswa`, `nisn`, `nama`, `id_angkatan`, `id_jurusan`, `id_kelas`, `alamat`) VALUES
-(1, '20220801113335', 'W', '2020/2021', 1, 5, 'Asdasd'),
-(2, '20220801113359', 'R', '2021/2022', 1, 14, 'Adsds'),
-(3, '20220801115635', 'Rty', '2021/2022', 1, 14, 'Sdfsfsdfsdf');
+INSERT INTO `siswa` (`id_siswa`, `nisn`, `nama`, `id_angkatan`, `id_jurusan`, `id_kelas`, `alamat`, `jenis_kelamin`, `ttl`) VALUES
+(14, '20241123100453', 'Muhammad Gilang Hamdani', '2020/2021', 4, 18, 'Mawar\r\n', 'laki-laki', 'Jakarta, 17-01-2007');
 
 -- --------------------------------------------------------
 
@@ -222,7 +229,7 @@ CREATE TABLE `siswatemp` (
   `kls11` varchar(10) NOT NULL,
   `kls12` varchar(10) NOT NULL,
   `tahun` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `siswatemp`
@@ -231,7 +238,15 @@ CREATE TABLE `siswatemp` (
 INSERT INTO `siswatemp` (`nisn`, `kls10`, `kls11`, `kls12`, `tahun`) VALUES
 ('20220801113335', 'X AKL 2', '', '', '2020/2021'),
 ('20220801113359', '', 'XI MM 2', 'XII MM 1', '2020/2021'),
-('20220801115635', '', '', 'XII MM 1', '2021/2022');
+('20220801115635', '', '', 'XII MM 1', '2021/2022'),
+('20241119042343', '', '', 'XII MM 1', '2021/2022'),
+('20241119043449', '', '', 'XII RPL', '2022/2023'),
+('20241119043714', '', '', 'XII RPL', '2022/2023'),
+('20241119044342', '', '', 'XII RPL', '2022/2023'),
+('20241120094009', 'X AKL 1', '', '', '2020/2021'),
+('20241120110155', 'X PPLG', '', '', '2020/2021'),
+('20241120164252', '', 'XI PPLG', '', '2021/2022'),
+('20241123100453', '', '', 'XII RPL', '2020/2021');
 
 --
 -- Indexes for dumped tables
@@ -268,6 +283,12 @@ ALTER TABLE `pembayaran`
   ADD PRIMARY KEY (`idspp`);
 
 --
+-- Indeks untuk tabel `pengeluaran`
+--
+ALTER TABLE `pengeluaran`
+  ADD PRIMARY KEY (`id_pengeluaran`);
+
+--
 -- Indeks untuk tabel `siswa`
 --
 ALTER TABLE `siswa`
@@ -299,25 +320,31 @@ ALTER TABLE `angkatan`
 -- AUTO_INCREMENT untuk tabel `jurusan`
 --
 ALTER TABLE `jurusan`
-  MODIFY `id_jurusan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_jurusan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT untuk tabel `kelas`
 --
 ALTER TABLE `kelas`
-  MODIFY `id_kelas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id_kelas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT untuk tabel `pembayaran`
 --
 ALTER TABLE `pembayaran`
-  MODIFY `idspp` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+  MODIFY `idspp` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=181;
+
+--
+-- AUTO_INCREMENT untuk tabel `pengeluaran`
+--
+ALTER TABLE `pengeluaran`
+  MODIFY `id_pengeluaran` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `siswa`
 --
 ALTER TABLE `siswa`
-  MODIFY `id_siswa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_siswa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
